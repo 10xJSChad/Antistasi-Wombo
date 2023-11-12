4e6a3e3ef71bf7eedbeded3c874dad3ea3cf2bb3
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2023-11-12 13:34:39 +0100

    correct-er loonix build script

--- build.sh
+++ build.sh
@@ -9,11 +9,14 @@ proton="proton"
 addonbuilder="/custom/media/1TB_EXT4_SSD/SteamLibrary/steamapps/common/Arma 3 Tools/AddonBuilder/AddonBuilder.exe"
 addon="$(pwd)/A3A/addons/$1"
 output="$(pwd)/build/@A3U/addons"
+addonname="$1"
 
 if [ "$1" = "all" ]; then
     for addon in "$(pwd)/A3A/addons/"*; do
         if [ -d "$addon" ]; then
-            proton run "$addonbuilder" "$addon" "$output" -packonly -prefix=x\\A3A\\addons\\"$(basename "$addon")"
+            addonname="$(basename "$addon")"
+            echo "Building $addonname"
+            "$proton" run "$addonbuilder" "$addon" "$output" -packonly -prefix=x\\A3A\\addons\\"$addonname" > /dev/null 2>&1
         fi
     done
     exit 0
@@ -25,4 +28,6 @@ if [ ! -d "$addon" ]; then
     exit 1
 fi
 
-"$proton" run "$addonbuilder" "$addon" "$output" -packonly -prefix=x\\A3A\\addons\\config_fixes
+# build addon
+echo "Building $addonname"
+"$proton" run "$addonbuilder" "$addon" "$output" -packonly -prefix=x\\A3A\\addons\\"$addonname" > /dev/null 2>&1
