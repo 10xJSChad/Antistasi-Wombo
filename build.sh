e6cb61c4d1a71fc012b12862d6152ab43d08b996
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2023-11-12 13:25:57 +0100

    loonix build script

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+proton run "/custom/media/1TB_EXT4_SSD/SteamLibrary/steamapps/common/Arma 3 Tools/AddonBuilder/AddonBuilder.exe" $@
+echo "proton run \"/custom/media/1TB_EXT4_SSD/SteamLibrary/steamapps/common/Arma 3 Tools/AddonBuilder/AddonBuilder.exe\" $@" >> log
